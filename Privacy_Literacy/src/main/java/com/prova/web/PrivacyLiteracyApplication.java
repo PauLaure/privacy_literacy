fc1f9f0d504b40bc6a37d07be0fc51bb9a3a68d3
Alessia Crispo <a.crispo6@studenti.unisa.it>
2023-03-21 20:29:51 +0100

    primo commit Ale

---
+++ Privacy_Literacy/src/main/java/com/prova/web/PrivacyLiteracyApplication.java
@@ -0,0 +1,13 @@
+package com.prova.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyLiteracyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacyLiteracyApplication.class, args);
+	}
+
+}
