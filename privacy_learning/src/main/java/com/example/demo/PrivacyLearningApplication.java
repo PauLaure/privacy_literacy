23b9cc18650dcc9d622d87be198e717b6cd5eca5
Aless <a.crispo6@studenti.unisa.it>
2023-03-28 12:42:00 +0200

    prova alessia da spring

---
+++ privacy_learning/src/main/java/com/example/demo/PrivacyLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacyLearningApplication.class, args);
+	}
+
+}
